b5ff5b6dad06eda2ac10f8520d3c45f8d87afc57
Adrian Ottiker <adrian@ottiker.com>
2014-01-12 00:52:35 +0100

    jlx-layout renders now views with miid config data

--- layout.js
+++ layout.js
@@ -1,6 +1,86 @@
 M.wrap('github/jillix/jlx-layout/v0.0.1/layout.js', function (require, module, exports) {
 
-var V = require('github/jillix/jlx-view/v0.0.1/view');
+var View = require('github/jillix/jlx-view/v0.0.1/view');
+
+var config = {
+    "title": "Mono Dev",
+    "view": {
+        "src": "viewTemplateId",
+        "views": [{
+            "id": "",
+            "to": "body"
+        }]
+    },
+    //"query": {},
+    /*
+        // custom code
+        M.custom = {
+            view: {
+                addCount: function (data) {
+                    this.count = this.count || 0;
+                    data.title = data.title + ' ' + (++this.count);
+                    return data;
+                },
+                resetCount: function (html) {
+                    this.count = 0;
+                }
+            }
+        };
+        
+        // view config
+        "on": {
+            "data": "M.custom.view.addCount",
+            "done": "M.custom.view.resetCount"
+        }
+    */
+    "data": [
+        {title: 'Rendered with jlx-view', text: 'jlx-view binds content dynamically to a html snippet'},
+        {title: 'Rendered with jlx-view', text: 'jlx-view binds content dynamically to a html snippet'},
+        {title: 'Rendered with jlx-view', text: 'jlx-view binds content dynamically to a html snippet'}
+    ]
+};
+
+function setupView (err, view) {
+    var self = this;
+    //var config = self.mono.config;
+    
+    if (err) {
+        return self.emit('view', err);
+    }
+    
+    // save view
+    self.views.push(view);
+    
+    // read data from server
+    if (config.query) {
+        /* TODO get data with crud
+        crud.read(config.query, function (err, data) {
+            
+            // render html
+            view.render();
+            
+        });*/
+    
+    // set data
+    } else {
+        
+        // set data
+        if (config.data) {
+            view.data = config.data;
+        }
+        
+        // append custom handlers
+        if (config.on) {
+            for (var event in config.on) {
+                view.on[event] = Object.path(config.on[event]);
+            }
+        }
+        
+        // render html and append it to the dom
+        view.render();
+        self.emit('view', null, view);
+    }
+}
 
 function init () {
     var self = this;
@@ -12,31 +92,27 @@ function init () {
     }
     
     // init view
-    if (config.view) {
-        V(self)(config.view, function (err, layout) {
-            
-            // read data from server
-            if (config.query) {
-                layout.read(config.query, function (err, data) {
-                    
-                    // render html
-                    layout.render(true);
-                    self.emit('ready');
-                });
-            
-            // set data
-            } else {
-                
-                // set data
-                if (config.data) {
-                    layout.set(config.data);
-                }
-            
-                // render html and append it to the dom
-                layout.render(true);
+    if (config.view && config.view.src && config.view.views) {
+        
+        self.views = [];
+        
+        // init view module
+        var V = View(config.view.src, self);
+        
+        // emit ready when all views are loaded
+        var count = 0;
+        var viewCounter = function (err, view) {
+            if (++count === config.view.views.length) {
                 self.emit('ready');
             }
-        });
+        };
+        self.on('view', viewCounter);
+        
+        // load views
+        for (var i = 0; i < config.view.views.length; ++i) {
+            V(config.view.views[i], setupView);
+        }
+        
     } else {
         self.emit('ready');
     }
