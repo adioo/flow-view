aa5296514fe01b4211477df69ef57b17f1a9e97b
Adrian Ottiker <adrian@ottiker.com>
2014-01-16 21:32:30 +0100

    using and testing the new bind lib module

--- layout.js
+++ layout.js
@@ -1,19 +1,69 @@
 M.wrap('github/jillix/jlx-layout/v0.0.1/layout.js', function (require, module, exports) {
 
-var Crud = require('github/jillix/jlx-crud/v0.0.1/crud');
-var View = require('github/jillix/jlx-view/v0.0.1/view');
+var Bind = require('github/jillix/bind/v0.0.1/bind');
 
-var config = {
-    "title": "Mono Dev",
-    "view": {
-        "views": [{
-            "id": "viewId",
-            "to": "body"
-        }]
+function ready (err, bind) {
+    var self = this;
+    var config = self.mono.config;
+    
+    if (err) {
+        return self.emit('error', err);
+    }
+    
+    // append custom handlers
+    if (config.on) {
+        for (var event in config.on) {
+            bind.on[event] = Object.path(config.view.on[event]);
+        }
+    }
+    
+    // set data
+    if (config.data) {
+        bind.data = config.data;
+    }
+    
+    // read data
+    if (config.read) {
+        bind.read(config.read, function (err, data) {
+            
+            if (err) {
+                return;
+            }
+            
+            if (data) {
+                bind.data = data;
+            }
+            
+            // render
+            bind.render();
+            self.emit('ready');
+        });
+    } else {
+        
+        // render
+        bind.render();
+        self.emit('ready');
+    }
+}
+
+function init () {
+    var self = this;
+    var config = self.mono.config = {
+        "title": "Mono Dev",
+        "bind": "bindId",
+        "to": "body",
+        "data": [
+            {title: 'Rendered with bind', text: 'Bind binds content dynamically to a html snippet', 'test': {'dot': {'notation': 'deep value'}}},
+            {title: 'Rendered with bind', text: 'Bind binds content dynamically to a html snippet', 'test': {'dot': {'notation': 'deep value'}}},
+            {title: 'Rendered with bind', text: 'Bind binds content dynamically to a html snippet', 'test': {'dot': {'notation': 'deep value'}}}
+        ],
+        /*"read": {
+            "q": {}
+        },*/
         /*
             // custom code
             M.custom = {
-                view: {
+                bind: {
                     addCount: function (data) {
                         this.count = this.count || 0;
                         data.title = data.title + ' ' + (++this.count);
@@ -25,109 +75,21 @@ var config = {
                 }
             };
             
-            // view config
+            // bind config
             "on": {
-                "data": "M.custom.view.addCount",
-                "done": "M.custom.view.resetCount"
+                "data": "M.custom.bind.addCount",
+                "done": "M.custom.bind.resetCount"
             }
         */
-    },
-    "source": "sourceId",
-    /*"read": {
-        "q": {}
-    },*/
-    "data": [
-        {title: 'Rendered with jlx-view', text: 'jlx-view binds content dynamically to a html snippet', 'test.dot.notation': 'this.is.a.dot.notation.field'},
-        {title: 'Rendered with jlx-view', text: 'jlx-view binds content dynamically to a html snippet', 'test.dot.notation': 'this.is.a.dot.notation.field'},
-        {title: 'Rendered with jlx-view', text: 'jlx-view binds content dynamically to a html snippet', 'test.dot.notation': 'this.is.a.dot.notation.field'}
-    ]
-};
-
-function setupView (err, view) {
-    var self = this;
-    //var config = self.mono.config;
-    
-    if (err) {
-        return self.emit('view', err);
-    }
-    
-    // save view
-    self.views.push(view);
-    
-    // append custom handlers
-    if (config.view.on) {
-        for (var event in config.view.on) {
-            view.on[event] = Object.path(config.view.on[event]);
-        }
-    }
-    
-    // read data from server
-    if (self.crud && config.read) {
-        self.crud.read(config.read, function (err, data) {
-            
-            if (err) {
-                return;
-            }
-            
-            // set data
-            if (data) {
-                view.data = data;
-            }
-            
-            // render html
-            view.render();
-        });
-    
-    // set data
-    } else {
-        
-        // set data
-        if (config.data) {
-            view.data = config.data;
-        }
-        
-        // render html and append it to the dom
-        view.render();
-        self.emit('view', null, view);
-    }
-}
-
-function init () {
-    var self = this;
-    //var config = self.mono.config;
+    } || self.mono.config;
     
     // set document title
     if (config.title) {
         document.title = config.title;
     }
     
-    // init view
-    if (config.view && config.view.views) {
-
-        self.views = [];
-
-        // init view module
-        var V = View(self);
-
-        // get crud instance
-        if (config.source) {
-            self.crud = Crud(self)(config.source);
-        }
-
-        // emit ready when all views are loaded
-        var count = 0;
-        var viewCounter = function (err, view) {
-            if (++count === config.view.views.length) {
-                self.emit('ready');
-            }
-        };
-        self.on('view', viewCounter);
-
-        // load views
-        for (var i = 0; i < config.view.views.length; ++i) {
-            V(config.view.views[i], setupView);
-        }
-
+    if (config.bind && config.to) {
+        Bind(self)(config.bind, config.to, ready);
     } else {
         self.emit('ready');
     }
