2ebeb16e94235e5d3ce8b84adb461c179f48ef50
Adrian Ottiker <adrian@ottiker.com>
2014-02-01 23:46:56 +0100

    renamed list module to table

---
+++ table.js
@@ -0,0 +1,156 @@
+M.wrap('github/jillix/table/v0.0.1/table.js', function (require, module, exports) {
+
+var View = require('github/jillix/view/v0.0.1/view');
+
+function createTemplates (schema) {
+    
+    // create headers and rows
+    var columns = {
+        headers: '<tr>',
+        rows: '<tr data="">'
+    };
+    
+    for (var field in schema) {
+        columns.headers += '<th>' + field + '</th>';
+        columns.rows += '<td>{' + field + '}</td>';
+    }
+    columns.headers += '</tr>';
+    columns.rows += '</tr>';
+    
+    return columns;
+}
+
+function clickRowHander (state, source, id) {
+    return function () {
+        state.emit('/editor/' + source + '/' + id);
+    };
+}
+
+function load () {
+    var self = this;
+    
+    // render items
+    self.item.model(getModelFromUrl(), function (err, model) {
+        
+        if (err || !model) {
+            return;
+        }
+        
+        self.model = model;
+        
+        // render title
+        if (self.title) {
+            self.title.template.render([{title: model.name}]);
+        }
+        
+        var templates = createTemplates(model.schema);
+        
+        // render header
+        if (self.head) {
+            self.head.template.set(templates.headers);
+            self.head.template.render();
+        }
+        
+        if (self.item) {
+            self.item.template.set(templates.rows);
+            model.read({q: {}, o: {}}, function (err, data) {
+                
+                if (err || !data) {
+                    data = [err] || ['no data'];
+                }
+                
+                self.item.template.render(data);
+                
+                // add clicks to rows
+                var rows = self.item.template.dom.querySelectorAll('tr');
+                for (var i = 0; i < rows.length; ++i) {
+                    rows[i].addEventListener('click', clickRowHander(self.item.state, self.model.name, data[i]._id), false);
+                }
+            });
+        }
+    });
+}
+
+function getModelFromUrl (model) {
+    return location.pathname.split('/')[2];
+}
+
+function init () {
+    var self = this;
+    var config = self.mono.config.data;
+    
+    // state handler
+    self.load = load;
+    
+    // init view
+    if (config && config.layout) {
+        var V = View(self);
+        
+        V.load(config.layout, function (err, layout) {
+            
+            if (err) {
+                return;
+            }
+            
+            // save view instance
+            self.layout = layout;
+            
+            if (self.layout.template) {
+                
+                // render layout view
+                self.layout.template.render();
+                
+                // load controller
+                var count = 0;
+                var handler = function (err) {
+                    if (++count === 3) {
+                        self.layout.model(getModelFromUrl(), function (err, model) {
+                            
+                            if (!err) {
+                                layout.state.emit();
+                            }
+                            
+                            self.emit('ready');
+                        });
+                    }
+                };
+                
+                // load title
+                if (config.title) {
+                    V.load(config.title, function (err, title) {
+                        self.title = title;
+                        handler(err);
+                    });
+                } else {
+                    handler();
+                }
+                
+                // load headers
+                if (config.head) {
+                    V.load(config.head, function (err, head) {
+                        self.head = head;
+                        handler(err);
+                    });
+                } else {
+                    handler();
+                }
+                
+                // load items
+                if (config.item) {
+                    V.load(config.item, function (err, item) {
+                        self.item = item;
+                        handler(err);
+                    });
+                } else {
+                    handler();
+                }
+            }
+        });
+    }
+}
+
+module.exports = init;
+
+return module;
+
+});
