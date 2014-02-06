ed45e161463236b83cdf5c26d2319911840eac8d
Luca Boieru <luca.boieru@yahoo.com>
2014-02-06 14:50:15 +0200

    Updated the animation to support standard show/hide.

--- layout.js
+++ layout.js
@@ -9,43 +9,74 @@ function page (config, page) {
 
     // animations
     if (config) {
+
         var view = this.view;
 
         var oldState = $(self.mono.config.data.selector + "[active]");
         var newState = $("#" + page.page);
 
-        var inAnimation = config.animations.inAnimation.effect;
+        // getting the in animation
+        if (!config.animations.inAnimation || config.animations.inAnimation === "none") {
+            var inAnimation = "fadeIn";
+            var inDuration = "0s";
+            var inDelay = "0s";
+        } else {
+            var inAnimation = config.animations.inAnimation.effect;
+            var inDuration = config.animations.inAnimation.duration || ".5s";
+            var inDelay = config.animations.inAnimation.delay || "0s";
+        }
+
+        // getting the current out animation
         var outAnimation = oldState.attr("active");
         var outDuration = oldState.attr("duration");
         var outDelay = oldState.attr("delay");
 
         oldState.removeAttr("active");
-        newState.attr("active", config.animations.outAnimation.effect);
-        newState.attr("duration", config.animations.outAnimation.duration);
-        newState.attr("delay", config.animations.outAnimation.delay);
-        
+        oldState.removeAttr("duration");
+        oldState.removeAttr("delay");
+
+        // setting the future out animation
+        if (!config.animations.outAnimation || config.animations.outAnimation === "none") {
+            newState.attr("active", "fadeOut");
+            newState.attr("duration", "0s");
+            newState.attr("delay", "0s");
+        } else {
+            newState.attr("active", config.animations.outAnimation.effect);
+            newState.attr("duration", config.animations.outAnimation.duration || ".5s");
+            newState.attr("delay", config.animations.outAnimation.delay || "0s");
+        }
+
+        // hiding all elements
         $(self.mono.config.data.selector).hide();
 
+        // there is no old state (the first time a page is loaded)
         if (oldState.length === 0) {
-            newState.css("-webkit-animation-duration", config.animations.inAnimation.duration);
-            newState.css("-webkit-animation-delay", config.animations.inAnimation.delay);
+            newState.css("-webkit-animation-duration", inDuration);
+            newState.css("-webkit-animation-delay", inDelay);
             newState.addClass("animated " + inAnimation);
             newState.show();
         } else {
-            
-            oldState.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-                oldState.hide();
-                oldState.removeClass("animated " + inAnimation + " " + outAnimation);
-                newState.css("-webkit-animation-duration", config.animations.inAnimation.duration);
-                newState.css("-webkit-animation-delay", config.animations.inAnimation.delay);
+            if (outDuration === "0s") {
+                newState.removeClass("animated " + inAnimation + " " + outAnimation);
+                newState.css("-webkit-animation-duration", inDuration);
+                newState.css("-webkit-animation-delay", inDelay);
                 newState.addClass("animated " + inAnimation);
                 newState.show();
-                
-            });
-            oldState.css("-webkit-animation-duration", outDuration);
-            oldState.css("-webkit-animation-delay", outDelay);
-            oldState.addClass("animated " + outAnimation);
-            oldState.show();
+            } else {
+                // out animation is done
+                oldState.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+                    oldState.hide();
+                    oldState.removeClass("animated " + inAnimation + " " + outAnimation);
+                    newState.css("-webkit-animation-duration", inDuration);
+                    newState.css("-webkit-animation-delay", inDelay);
+                    newState.addClass("animated " + inAnimation);
+                    newState.show();
+                });
+                oldState.css("-webkit-animation-duration", outDuration);
+                oldState.css("-webkit-animation-delay", outDelay);
+                oldState.addClass("animated " + outAnimation);
+                oldState.show();
+            }
         }
 
     }
