c9a2d92fce63dc2f6091cc9e9d946dbe7086e01e
Adrian Ottiker <adrian@ottiker.com>
2014-03-05 10:56:09 +0100

    try to init custom module with scope of the mono instance

--- joint.js
+++ joint.js
@@ -3,7 +3,12 @@ M.wrap('github/jillix/joint/v0.0.1/joint.js', function (require, module, exports
 function init () {
     var self = this;
     
-    config = self.mono.config.data;
+    var config = self.mono.config.data;
+    var module = M.path(config.module);
+    
+    if (typeof module === 'function') {
+        module.call(self, config.config);
+    }
 }
 
 module.exports = init;
