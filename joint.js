5b056c77ba2825f40102a13f70dd4d481d7989b9
Adrian Ottiker <adrian@ottiker.com>
2014-03-05 10:00:03 +0100

    created basic module structure

---
+++ joint.js
@@ -0,0 +1,13 @@
+M.wrap('github/jillix/joint/v0.0.1/joint.js', function (require, module, exports) {
+
+function init () {
+    var self = this;
+    
+    config = self.mono.config.data;
+}
+
+module.exports = init;
+
+return module;
+
+});
---
+++ server/joint.js
@@ -0,0 +1,7 @@
+var M = process.mono;
+
+function init (config) {
+    
+}
+
+module.exports = init;
