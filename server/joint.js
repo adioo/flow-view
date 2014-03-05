0cc64779997b9182827421eda7fd655ac2a9d59e
Adrian Ottiker <adrian@ottiker.com>
2014-03-05 10:55:36 +0100

    require custom module and init it with the scope of the mono instance

--- server/joint.js
+++ server/joint.js
@@ -1,7 +1,16 @@
 var M = process.mono;
 
 function init (config) {
+    var self = this;
     
+    // load and init custom module
+    if (config.module) {
+        try {
+            (require(M.paths.APPLICATION_ROOT + module)).call(self, config.config);
+        } catch (err) {
+            // TODO handle error
+        }
+    }
 }
 
 module.exports = init;
