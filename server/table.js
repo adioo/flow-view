ac26cc52ed04ba2c139931a9980a30177e23d785
Adrian Ottiker <adrian@ottiker.com>
2014-04-07 14:39:36 +0200

    testing new view implementation

--- server/table.js
+++ server/table.js
@@ -4,9 +4,9 @@ var View = require(M.config.paths.MODULE_ROOT + 'github/jillix/view/v0.0.1/serve
 function init (config) {
     var self = this;
     
-    View(this, function (err) {
-        self.emit('ready');
-    });
+    View(self);
+    
+    self.emit('ready');
 }
 
 module.exports = init;
