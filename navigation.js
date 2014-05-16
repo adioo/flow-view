a93f591ed145112f4846f9a9a54876945e90eab5
Adrian Ottiker <adrian@ottiker.com>
2014-05-17 01:32:27 +0200

    updated module to use the new view/model api

--- navigation.js
+++ navigation.js
@@ -1,7 +1,5 @@
 M.wrap('github/jillix/navigation/v0.0.1/navigation.js', function (require, module, exports) {
 
-var View = require('github/jillix/view/v0.0.1/view');
-
 // emit dom states
 function domStateHandler (self, view, config) {
     
@@ -50,35 +48,11 @@ function activate (state) {
     }
     
     // remove the active class
-    $('.active', self.layout.template.dom).removeClass('active');
+    $('.active', self.layout.dom).removeClass('active');
 
     // add the active class to the active navigation tab
     if (self.active) {
         $(self.active).addClass('active');
-    }
-}
-
-function loadModel (config, item, callback) {
-
-    switch (typeof config.model) {
-
-        // model by name
-        case 'string':
-            item.model(config.model, function (err, model) {
-                model.read({q: config.query || {}}, function (err, data) {
-                    callback(err, data);
-                });
-            });
-            return;
-
-        // model data
-        case 'object':
-            callback(null, config.model);
-            return;
-
-        default:
-            callback('Invalid navigation model');
-            return;
     }
 }
 
@@ -98,11 +72,12 @@ function getActiveNameFromUrl (pattern) {
 function init () {
     var self = this;
     var config = self.mono.config.data;
-    var V = View(self);
 
     if (!config) {
         return console.error('[nav: no config available]');
     }
+    
+    config.labelKey = config.labelKey || 'label';
 
     self.config = config;
     
@@ -111,7 +86,7 @@ function init () {
     if (config.baseLength < 0) {
         config.baseLength = 0;
     }
-    self.base = location.pathname.split('/').slice(0, config.baseLength + 1).join('/') + '/';
+    self.base = (location.pathname.split('/').slice(0, config.baseLength + 1).join('/') + '/').replace(/\/\//, '/');
     
     // get current active
     var active = null;
@@ -120,99 +95,88 @@ function init () {
         active = getActiveNameFromUrl(config.pattern);
     }
     
-    // parse query
-    if (config.query) {
-        config.query = JSON.parse(config.query);
-    }
-    
     // init layout view
-    V.load(config.layout, function (err, layout) {
+    self.view(config.layout, function (err, layout) {
 
         if (err) {
             return console.error('[nav: ' + err.toString() + ']');
         }
         
         // return when no dom is available
-        if (!layout.template && layout.template.dom) {
+        if (!layout && layout.dom) {
             return console.error('[nav: no dom available]');
         }
         
+        // render html
+        layout.render();
         self.layout = layout;
         
-        // render html
-        layout.template.render();
-        
         // init item view
-        V.load(config.item, function (err, item) {
+        self.view(config.item, function (err, item) {
 
             if (err) {
                 return console.error('[nav: ' + err.toString() + ']');
             }
             
             // return when no dom is available
-            if (!item.template && item.template.dom) {
+            if (!item && item.dom) {
                 return console.error('[nav item: no dom available]');
             }
-
-            loadModel(config, item, function(err, data) {
             
-                if (err) {
-                    return console.error('[nav: ' + err.toString() + ']');
+            self.item = item;
+            
+            var data = item.config.items;
+                
+            // check if stateKey exists
+            if (!data[0] || typeof data[0][config.stateKey] === 'undefined') {
+                 return console.error('[nav: No url key found]');
+            }
+            
+            // render items
+            item.render(data);
+            
+            // add change state handler to nav items
+            var i = 0;
+            var items = $('li', item.dom);
+            self.items = {};
+            
+            for (i = 0; i < items.length; ++i) {
+                var $item = $(items[i]);
+                
+                // save state item
+                self.items[data[i][config.stateKey]] = $item;
+                
+                // save active item if found in the url
+                if (data[i][config.stateKey] === active) {
+                    self.active = $item;
                 }
                 
-                self.item = item;
-                
-                // check if stateKey exists
-                if (!data[0] || typeof data[0][config.stateKey] === 'undefined') {
-                     return console.error('[nav: No url key found]');
-                }
-                
-                // render items
-                item.template.render(data);
-                
-                // add change state handler to nav items
-                var i = 0;
-                var items = $('li', item.template.dom);
-                self.items = {};
-                
-                for (i = 0; i < items.length; ++i) {
-                    var $item = $(items[i]);
-                    
-                    // save state item
-                    self.items[data[i][config.stateKey]] = $item;
-                    
-                    // save active item if found in the url
-                    if (data[i][config.stateKey] === active) {
-                        self.active = $item;
-                    }
-                    
-                    $item.on('click', domStateHandler(self, item, data[i]));
-                }
-                
-                // emit state events on dom elms
-                if (config.domStates) {
-                    for (i = 0; i < config.domStates.length; ++i) {
-                        // get element
-                        var elm = $(config.domStates[i].selector, layout.template.dom);
-                        if (elm) {
-                            
-                            // save state items
-                            self.items[config.domStates[i].state] = elm;
-                            
-                            // save active item if found in the url
-                            if (config.domStates[i].state === active){
-                                self.active = elm;
-                            }
-                            
-                            elm.on(config.domStates[i].event, domStateHandler(self, item, config.domStates[i]));
+                $item.on('click', domStateHandler(self, item, data[i]));
+            }
+            
+            // emit state events from dom elms
+            if (config.domStates) {
+                for (i = 0; i < config.domStates.length; ++i) {
+                    // get element
+                    var elm = $(config.domStates[i].selector, layout.dom);
+                    if (elm) {
+                        
+                        // save state items
+                        self.items[config.domStates[i].state] = elm;
+                        
+                        // save active item if found in the url
+                        if (config.domStates[i].state === active){
+                            self.active = elm;
                         }
+                        
+                        elm.on(config.domStates[i].event, domStateHandler(self, item, config.domStates[i]));
                     }
                 }
-                
-                // add activate item handler to state
-                self.on('route', activate);
-                self.emit('ready');
-            });
+            }
+            
+            // add activate item handler to state
+            self.on('route', activate);
+            self.emit('ready');
         });
     });
 }
