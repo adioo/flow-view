98b246c5542e03c523f3be04b537cd3cdf4031b1
Adrian Ottiker <adrian@ottiker.com>
2014-08-04 09:07:27 +0200

    updated to new engine config

--- layout.js
+++ layout.js
@@ -1,7 +1,64 @@
 Z.wrap('github/jillix/layout/v0.0.1/layout.js', function (require, module, exports) {
 
-function page (state, target, options) {
+module.exports = init;
+
+/*
+    type: constructor
+*/
+function init (config, ready) {
     var self = this;
+    var pageName = '_page_' + self._name;
+
+    // set document title
+    if (config.title) {
+        self.title = config.title;
+        document.title = config.title;
+    }
+
+    // attach state handler to instance
+    self.page = page;
+    self.transition = transition;
+    self.render = render;
+    self.$jq = $jq;
+
+    // state handler to handle css in pages
+    self.pageSelector = '.' + pageName;
+
+    // render views
+    if (self.view && self.view.layout) {
+
+        // render layout and add page classes
+        self.view.layout.render([{page: pageName}]);
+
+        // get pages dom refs
+        self.pages = $(self.pageSelector, self.view.layout.dom);
+
+        // hide all pages in init state
+        self.pages.hide();
+
+        // handle not found
+        if ((self.notFound = $(config.notFound, self.view.layout.dom))) {
+            self.on('route', notFoundHandler);
+        }
+
+        // render other views
+        for (var view in self.view) {
+            if (view !== 'layout') {
+                self.view[view].render();
+            }
+        }
+    }
+
+    ready();
+}
+
+/*
+    type: actor
+*/
+function page (state, data) {
+    var self = this;
+    var target = data.show;
+    var options = data.options;
 
     // return when no target page is given
     if (!target) {
@@ -37,13 +94,23 @@ function page (state, target, options) {
     }
 }
 
-// TODO implement animations
-function transition (state, from, to) {
-    $(from).hide();
-    $(to).show();
+/*
+    type: actor
+    TODO: implement animations
+*/
+function transition (state, data) {
+
+    if (!data) {
+        return;
+    }
+
+    data.hide && $(data.hide).hide();
+    data.show && $(data.show).show();
 }
 
-// animate page transitions
+/*
+    type: private
+*/
 function animate (elm, config) {
     var self = this;
 
@@ -81,48 +148,36 @@ function animate (elm, config) {
     }
 }
 
-function animation_end_handler (elm, animate_class) {
-    return function () {
-        elm.removeClass(animate_class);
-    };
-}
-
-// not found handler
-// TODO needs to be tested
-function notFoundHandler (state) {
+/*
+    type: actor
+*/
+function render (state, data, view) {
     var self = this;
 
-    if (self._url === state.url) {
+    if (!self.view || !self.view[view] || !data) {
         return;
     }
 
-    if (!self._state) {
-        self.pages.hide();
-        self.notFound.show();
-    }
+    // render data
+    //self.view[view].render([data]);
 
-    self._state = false;
-}
-
-// render views on events
-function render (state) {
-    var self = this;
-
-    if (!self.view) {
-        return;
-    }
-
-    var views = self._toArray(arguments).slice(1);
+    /*var views = self._toArray(arguments).slice(1);
     for (var i = 0; i < views.length; ++i) {
         if (self.view[views[i]]) {
-            self.view[views[i]].render();
+            self.view[views[i]].render(data);
         }
-    }
+    }*/
 }
 
-// mainpulate dom with jquery
-function $jq (event, selector, method) {
+/*
+    type: actor
+    desc: mainpulate dom with jquery
+*/
+function $jq (event, data) {
     var self = this;
+    var selector = data.selector;
+    var method = data.method;
+    var args = data.args;
 
     // call jquery function
     if ($.fn[method]) {
@@ -154,63 +209,40 @@ function $jq (event, selector, method) {
             }
         }
 
-        var args = self._toArray(arguments).slice(3);
-
         // call method
         selector = $(selector);
         selector[method].apply(selector, args);
     }
 }
 
-function init (config, ready) {
-    var self = this;
-    var pageName = '_page_' + self._name;
-
-    // set document title
-    if (config.title) {
-        self.title = config.title;
-        document.title = config.title;
-    }
-
-    // attach state handler to instance
-    self.page = page;
-    self.render = render;
-    self.transition = transition;
-    self.$jq = $jq;
-
-    // state handler to handle css in pages
-    self.pageSelector = '.' + pageName;
-
-    // render views
-    if (self.view && self.view.layout) {
-
-        // render layout and add page classes
-        self.view.layout.render([{page: pageName}]);
-
-        // get pages dom refs
-        self.pages = $(self.pageSelector, self.view.layout.dom);
-
-        // hide all pages in init state
-        self.pages.hide();
-
-        // handle not found
-        if ((self.notFound = $(config.notFound, self.view.layout.dom))) {
-            self.on('route', notFoundHandler);
-        }
-
-        // render other views
-        for (var view in self.view) {
-            if (view !== 'layout') {
-                self.view[view].render();
-            }
-        }
-    }
-
-    console.log('layout:', self._name);
-    ready();
+/*
+    type: private
+*/
+function animation_end_handler (elm, animate_class) {
+    return function () {
+        elm.removeClass(animate_class);
+    };
 }
 
-module.exports = init;
+/*
+    type: private
+    TODO: needs to be tested
+*/
+function notFoundHandler (state) {
+    var self = this;
+
+    if (self._url === state.url) {
+        return;
+    }
+
+    if (!self._state) {
+        self.pages.hide();
+        self.notFound.show();
+    }
+
+    self._state = false;
+}
+
 
 return module;
 
