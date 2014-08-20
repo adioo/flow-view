1ff6bcbabeaa5e407a899ec9f21bc027fb4c0f58
Ionică Bizău <bizauionica@yahoo.com>
2014-08-20 10:14:35 +0200

    Added debugger

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ Z.wrap('github/jillix/joint/v0.0.1/joint.js', function (require, module, exports
     function init (config, ready) {
         var self = this;
 
+        debugger;
         // TODO
         ready();
     }
