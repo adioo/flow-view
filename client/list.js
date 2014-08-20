7346465fa32f2ac4e8dc0bc8b91a9e7e4b08f3a3
Ionică Bizău <bizauionica@yahoo.com>
2014-08-20 13:39:14 +0200

    Read operation

---
+++ client/list.js
@@ -0,0 +1,17 @@
+Z.wrap('github/ionicabizau/list/v0.0.1/client/list.js', function (require, module, exports) {
+
+    module.exports = function (self) {
+        var config = self._conf;
+        this.read = function (query, options, callback) {
+            self.model.req({
+                m: "find",
+                d: {
+                    q: query,
+                    o: options
+                }
+            }, callback);
+        }
+    };
+
+    return module;
+});
--- client/main.js
+++ client/main.js
@@ -1,10 +1,45 @@
-Z.wrap('github/jillix/joint/v0.0.1/joint.js', function (require, module, exports) {
+Z.wrap('github/ionicabizau/list/v0.0.1/client/main.js', function (require, module, exports) {
+
+    var List = require("./list");
 
     function init (config, ready) {
         var self = this;
+        self._conf = {
+            "ui": {
+                "template": {
+                    "selector": "",
+                    "class": "template"
+                },
+                "container": ""
+            },
+            "options": {
+                "query": {},
+                "options": {}
+            },
+            "model": "",
+            "autoinit": true
+        };
 
-        debugger;
-        // TODO
+        config = $.extend(self._conf, config);
+        var instanceSelector = Object.keys(self.view).join(",");
+        var $itemTemplate = $(self._conf.ui.template.selector, instanceSelector);
+        var list = new List(self);
+
+        self.model = self.model[self._conf.model];
+        if (!self.model) {
+            throw new Error("Model is not loaded. Please load the model using the composition configuration");
+        }
+
+        if (self._conf.autoinit) {
+            var req = config.options;
+            list.read(req.query, req.options, function () {
+                if (err) { return errorHandler(err); }
+                ready();
+            });
+        } else {
+            // TODO
+        }
+
         ready();
     }
 
