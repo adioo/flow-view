a2534c1d81f5369c27a61a02b9d93fcb8051d71b
Ionică Bizău <bizauionica@yahoo.com>
2014-08-21 09:58:15 +0200

    Render docs using the built-in render

--- client/ui.js
+++ client/ui.js
@@ -2,11 +2,9 @@ Z.wrap('github/ionicabizau/list/v0.0.1/client/ui.js', function (require, module,
 
     module.exports = function (self) {
         var config = self._conf;
+
         this.render = function (docs) {
-            for (var i = 0; docs.length; ++i) {
-                var cDoc = docs[i];
-                debugger;
-            }
+            self.view.item.render(docs);
         };
     };
 
